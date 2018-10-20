31909b3de6a3216d9bbe463d765e1482b7b2990d
ksereda <ksereda2017@gmail.com>
2018-10-20 18:32:51 +0300

    added "Convert Boolean to a String" task

---
+++ src/ConvertBoolenToString.java
@@ -0,0 +1,15 @@
+/*
+
+"Convert Boolean to a String"
+
+*/
+
+public class ConvertBoolenToString {
+    public static String convert(boolean b) {
+        return String.valueOf(b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convert(true));
+    }
+}
